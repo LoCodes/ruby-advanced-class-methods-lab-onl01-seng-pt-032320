f2571424b434bcaac8b55f2d47d7539d5a0cff6a
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-23 03:46:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,6 @@ class Song
   # .alphabetical
   # returns all the song instances in alphabetical order by song name
   def self.alphabetical
-    self.array.sort do |a, b|
-    end
-  end
+    self.sort{|song| song.name == name}
+  end 
+end 
