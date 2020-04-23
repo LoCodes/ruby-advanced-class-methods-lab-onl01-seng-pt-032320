abdb7974d5eed58e4a2e6b4440efac1b924bec2d
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-23 03:28:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,14 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
+  # .find_or_create_by_name
+  #  invokes .find_by_name and .create_by_name instead of repeating code (FAILED - 1)
+  #  returns the existing Song object (doesn't create a new one) when provided the title of an existing Song (FAILED - 2)
+  #  creates a new Song object with the provided title if one doesn't already exist
+
+  def self.find_or_create_by_name
+  end 
+
+  
+
 end
